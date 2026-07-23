4a4e745f8b89c988d697618aaf4ddebc6b1e9e80
MintyKeller <talitakeller8@gmail.com>
2026-07-23 18:21:35 -0300

    mod2

---
+++ MOD2/teste/dist/script.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// 1) Variáveis com tipos básicos
+const nome = "Minty";
+let idade = 25;
+const ativo = true;
+console.log("Nome:", nome);
+console.log("Idade:", idade);
+console.log("Ativo?", ativo);
+//# sourceMappingURL=script.js.map
---
+++ MOD2/teste/src/index.ts
@@ -0,0 +1,10 @@
+
+// 1) Variáveis com tipos básicos
+const nome: string = "Minty";
+let idade: number = 25;
+const ativo: boolean = true;
+console.log("Nome:", nome);
+console.log("Idade:", idade);
+console.log("Ativo?", ativo);
+
+
